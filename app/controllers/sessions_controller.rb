cd793d828da865821edaf5d9e732a87da935c289
shimar <shima01dev@gmail.com>
2016-04-24 23:40:54 +0900

    implement session actions.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,21 @@ class SessionsController < ApplicationController
   skip_before_action :require_login, except: [:destroy]
 
   def new
+    @user = User.new
     render layout: 'unsigned'
   end
 
   def create
+    if @user = login(params[:email], params[:password])
+      redirect_back_or_to(:users, notice: 'Login successful')
+    else
+      flash.now[:alert] = 'Login failed'
+      render action: 'new'
+    end
   end
 
   def destroy
+    logout
   end
 
 end
