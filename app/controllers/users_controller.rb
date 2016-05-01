ce3000f4d4815f91b3d465bd304ce86a580259ca
shimar <shima01dev@gmail.com>
2016-05-01 23:45:56 +0900

    implement the user registration.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,26 @@ class UsersController < ApplicationController
   skip_before_action :require_login
 
   def new
+    @user = User.new
     render layout: 'unsigned'
   end
 
   def create
-    render layout: 'unsigned'
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render action: 'new', layout: 'unsigned'
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user)
+      .permit(:email,
+              :password,
+              :password_confirmation)
+  end
+
 end
