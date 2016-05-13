d80aeba90cf5ebd558bf3935d62930f5db4b1713
shimar <shima01dev@gmail.com>
2016-05-14 01:00:20 +0900

    set the current date to item form.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @date = Time.zone.now
-    @item = ItemForm.new(user_id: current_user.id)
+    @item = ItemForm.new(user_id: current_user.id, date: @date)
   end
 end
