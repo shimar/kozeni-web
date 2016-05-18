e93b3b4763bf7dfedd32d9bdd16139bad4d955d7
shimar <shima01dev@gmail.com>
2016-05-19 02:18:33 +0900

    implement incomes create action.

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -8,12 +8,30 @@ class IncomesController < ApplicationController
   end
 
   def create
+    category_name = income_params[:category]
+    category = current_user.categories.find_or_create_by(name: category_name)
+    income = Income.new(user_id: current_user.id, date: income_params[:date], amount: income_params[:amount])
+    income.category = category
+    income.save
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def income_params
+    params.require(:item_form).permit(
+      :id,
+      :user_id,
+      :category,
+      :date,
+      :amount,
+      :planned
+    )
   end
 
 end
