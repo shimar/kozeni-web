e1287a45a3c3830186c9fb59401b9313debe8bd4
shimar <shima01dev@gmail.com>
2016-05-27 02:12:23 +0900

    impl to render the response.

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -10,9 +10,9 @@ class IncomesController < ApplicationController
   def create
     category_name = income_params[:category]
     category = current_user.categories.find_or_create_by(name: category_name)
-    income = Income.new(user_id: current_user.id, date: income_params[:date], amount: income_params[:amount])
-    income.category = category
-    income.save
+    @income = Income.new(user_id: current_user.id, date: income_params[:date], amount: income_params[:amount])
+    @income.category = category
+    @income.save
   end
 
   def update
---
+++ app/views/incomes/create.json.jbuilder
@@ -0,0 +1,9 @@
+json.(@income,
+      :id,
+      :user_id,
+      :category_id,
+      :date,
+      :amount,
+      :planned,
+      :created_at,
+      :updated_at)
