4f909cd1707de7f0c249e999c155859cbd6fdded
shimar <shima01dev@gmail.com>
2016-06-01 22:25:46 +0900

    write the spec of home controller.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,9 +3,22 @@ require 'rails_helper'
 RSpec.describe HomeController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    describe "when user signed in," do
+      before do
+        user_signed
+      end
+
+      it "returns http success" do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe "when user not signed in," do
+      it "redirects to welcome_path." do
+        get :index
+        expect(response).to redirect_to welcome_path
+      end
     end
   end
 
