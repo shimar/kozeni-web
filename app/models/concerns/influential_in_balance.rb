ad4da39980a7adf9d155338d060ca96d3ae66805
shimar <shima01dev@gmail.com>
2016-06-05 23:29:33 +0900

    add a skeleton file of InfluentialInBalance concern.

---
+++ app/models/concerns/influential_in_balance.rb
@@ -0,0 +1,12 @@
+module InfluentialInBalance
+  extend ActiveSupport::Concern
+
+  def self.find_or_create_monthly_balance
+    # TODO
+  end
+
+  def self.update_monthly_balance
+    # TODO
+  end
+
+end
