b09aa86ac81b89e98ea4491757f16848728ef443
shimar <shima01dev@gmail.com>
2016-06-13 01:36:37 +0900

    update the incomes controller.

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,24 +1,12 @@
 class IncomesController < ApplicationController
   layout false
 
-  def index
-  end
-
-  def show
-  end
-
   def create
     category_name = income_params[:category]
     category = current_user.categories.find_or_create_by(name: category_name)
     @income = Income.new(user_id: current_user.id, date: income_params[:date], amount: income_params[:amount])
     @income.category = category
     @income.save
-  end
-
-  def update
-  end
-
-  def destroy
   end
 
   private
--- spec/controllers/incomes_controller_spec.rb
+++ spec/controllers/incomes_controller_spec.rb
@@ -1,53 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe IncomesController, type: :controller do
-
-  describe "GET #index" do
-    describe "when a user signed in," do
+  describe "POST #create" do
+    context "when a user signed in," do
       before do
         user_signed
       end
 
-      it "returns http success" do
-        get :index
-        expect(response).to have_http_status(:success)
-      end
+      it "returns http success."
     end
 
-    describe "when no user signed in," do
-      it "redirects to welcome_path." do
-        get :index
-        expect(response).to redirect_to welcome_path
-      end
+    context "when no user signed in," do
+      it "redirects to welcome_path."
     end
   end
-
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
