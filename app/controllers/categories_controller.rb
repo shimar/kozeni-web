44570bd27e54390a6e19593aae2c574b09c1180e
shimar <shima01dev@gmail.com>
2016-07-11 23:33:29 +0900

    fix typo.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def index
     @categories = current_user.categories
-    @categories = @categories.where("name like ?", pamaras[:q] + '%') if params.key?(:q)
+    @categories = @categories.where("name like ?", params[:q] + '%') if params.key?(:q)
   end
 end
